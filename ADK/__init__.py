8210c7c0b97ce7fdede636af0d5b9ceb2c88016e
Srushanth Baride <SrushanthBaride@Gmail.com>
2025-05-03 15:05:36 +0100

    Add ADK package initialization files with module imports

---
+++ ADK/__init__.py
@@ -0,0 +1,12 @@
+"""ADK package"""
+
+# -*- coding: utf-8 -*-
+# **************************************************************************************************
+#
+#  🚀 Created by RocketML
+#  🖥️ Author: Srushanth Baride
+#  ✉️ Email: Srushanth.Baride@gmail.com
+#  📅 Date: 03-May-2025
+#  📖 Description: ADK package initialization
+#
+# **************************************************************************************************
---
+++ ADK/src/__init__.py
@@ -0,0 +1,16 @@
+"""ADK package"""
+
+# -*- coding: utf-8 -*-
+# **************************************************************************************************
+#
+#  🚀 Created by RocketML
+#  🖥️ Author: Srushanth Baride
+#  ✉️ Email: Srushanth.Baride@gmail.com
+#  📅 Date: 03-May-2025
+#  📖 Description: ADK package initialization
+#
+# **************************************************************************************************
+
+from . import agent
+
+__all__ = ["agent"]
