d37395850dcee77c07027c7992275d3e940d9dce
Srushanth Baride <SrushanthBaride@Gmail.com>
2025-05-16 16:43:20 +0100

    Add initial implementation of MCP Server with date and time tools

---
+++ MCP_Server/src/main.py
@@ -0,0 +1,34 @@
+"""MCP Server"""
+
+# -*- coding: utf-8 -*-
+# **************************************************************************************************
+#
+#  🚀 Created by Srushanth Baride
+#  🖥️ Author: Srushanth Baride
+#  ✉️ Email: Srushanth.Baride@gmail.com
+#  📅 Date: 03-May-2025
+#  📖 Description: TODO
+#
+# **************************************************************************************************
+
+import datetime
+import zoneinfo
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("clock")
+
+
+@mcp.tool()
+async def get_current_date_time_iso_format():
+    """Get the current date and time in ISO format"""
+    return datetime.datetime.now().isoformat()
+
+
+@mcp.tool()
+async def get_current_date_time_in_time_zone(time_zone: str):
+    """Get the current date and time in a specific time zone"""
+    return datetime.datetime.now(tz=zoneinfo.ZoneInfo(key=time_zone)).isoformat()
+
+
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
