85c263a08a65e5ad54a0e3e8c38a3f81304e13a0
Srushanth Baride <SrushanthBaride@Gmail.com>
2026-06-03 19:36:57 +0530

    feat: initialize MCP server project structure with main module boilerplate

--- mcp-server-directry-management/src/main.py
+++ mcp-server-directry-management/src/main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from mcp-server-directry-management!")
+"""MCP Server"""
 
-
-if __name__ == "__main__":
-    main()
+# -*- coding: utf-8 -*-
+# **************************************************************************************************
+#
+#  🚀 Created by Srushanth Baride
+#  🖥️ Author: Srushanth Baride
+#  ✉️ Email: Srushanth.Baride@gmail.com
+#  📅 Date: 03-June-2026
+#  📖 Description: TODO
+#
+# **************************************************************************************************
