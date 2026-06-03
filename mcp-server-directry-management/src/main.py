803a1c930511650ae2daca8f5b461b3ed3943f1d
Srushanth Baride <SrushanthBaride@Gmail.com>
2026-06-03 19:42:27 +0530

    feat: initialize MCP server with directory listing tool

--- mcp-server-directry-management/src/main.py
+++ mcp-server-directry-management/src/main.py
@@ -10,3 +10,26 @@
 #  📖 Description: TODO
 #
 # **************************************************************************************************
+
+import os
+from typing import List
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("directory-management")
+
+
+@mcp.tool()
+def list_files(directory_path: str) -> List[str]:
+    """Tool to list files in a directory
+
+    Args:
+        directory_path (str): Path to the directory
+
+    Returns:
+        List[str]: List of files in the directory
+    """
+    return os.listdir(path=directory_path)
+
+
+if __name__ == "__main__":
+    mcp.run(transport="http", host="localhost", port=2000)
