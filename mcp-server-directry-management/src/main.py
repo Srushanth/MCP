440fc0c3361b004ec105217f16ec7fa961c7212a
Srushanth Baride <SrushanthBaride@Gmail.com>
2026-06-03 20:10:30 +0530

    feat: initialize MCP server with directory management tools for listing items, files, and folders

--- mcp-server-directry-management/src/main.py
+++ mcp-server-directry-management/src/main.py
@@ -51,5 +51,24 @@ def list_files(directory_path: str) -> List[str]:
     return list_of_files
 
 
+@mcp.tool()
+def list_folders(directory_path: str) -> List[str]:
+    """Tool to list folders in a directory
+
+    Args:
+        directory_path (str): Path to the directory
+
+    Returns:
+        List[str]: List of folders in the directory
+    """
+    list_of_items = os.listdir(path=directory_path)
+    list_of_folders = [
+        item
+        for item in list_of_items
+        if os.path.isdir(os.path.join(directory_path, item))
+    ]
+    return list_of_folders
+
+
 if __name__ == "__main__":
     mcp.run(transport="stdio")
