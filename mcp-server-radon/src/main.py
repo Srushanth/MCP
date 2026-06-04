80ae4dd0271088db9276a21a708779df1af961f6
Srushanth Baride <SrushanthBaride@Gmail.com>
2026-06-04 19:47:43 +0530

    feat: implement Radon MCP server with tools for complexity and maintainability analysis

--- mcp-server-radon/src/main.py
+++ mcp-server-radon/src/main.py
@@ -45,3 +45,32 @@ def get_complexity_report(
             "--no-skip-over-tests",
         ],
     )
+
+
+@mcp.tool()
+def get_maintainability_index(
+    path: str = Field(..., description="Path to the file")
+) -> str:
+    """
+    Radon analyses Python source code and returns metrics for code-quality
+    assessment. It can be used as a package, module, or command-line tool.
+
+    Args:
+        path (str, optional): Path to the file. Defaults to Field(..., description="Path to the file").
+
+    Returns:
+        str: Maintainability Index of the file
+    """
+    return radon.cli.main_for_function(
+        radon.cli.CLI,
+        [
+            "mi",
+            path,
+            "-a",
+            "-s",
+            "--no-doc",
+            "--no-multiline",
+            "--no-blank",
+            "--no-skip-over-tests",
+        ],
+    )
